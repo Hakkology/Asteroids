fdf91281b2738993e146e5854570e335ae87673c
Hakkology <yildizhakan88@gmail.com>
2024-05-09 20:21:47 +0300

    Asteroids test.

---
+++ Assets/Scripts/Gameplay/Ship.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Class for the ship object.
+public class Ship : MonoBehaviour {
+
+	#region PUBLIC VARIABLES
+	// The rotation speed of the ship, in degrees per second.
+	public float rotationSpeed = 180f;
+
+	// The movement speed of the ship, in force applied per second.
+	public float movementSpeed = 500f;
+
+	// A reference to the transform where bullets will be spawned from.
+	public Transform launcher;
+	#endregion
+
+	#region PRIVATE VARIABLES
+	private const string TURN_COROUTINE_NAME = "TurnTowardsPointAndShootCoroutine";
+
+	private GameManager gameManager;
+
+	private bool turning = false;
+	#endregion
+
+	#region MONOBEHAVIOUR METHODS
+	void Start()
+	{
+		gameManager = GameManager.Instance;
+	}
+
+	private void OnEnable()
+	{
+		UserInputHandler.OnTap += TurnTowardsTouch;
+	}
+	private void OnDisable()
+	{
+		UserInputHandler.OnTap -= TurnTowardsTouch;
+	}
+
+	#endregion
+	
+	#region PUBLIC METHODS
+	public void OnHit()
+	{
+		gameManager.LoseLife();
+		StartCoroutine(StartInvincibilityTimer(2.5f));
+	}
+	#endregion
+
+	#region PRIVATE METHODS
+	private void TurnTowardsTouch (Touch t)
+	{
+
+		Vector3 targetPoint = Camera.main.ScreenToWorldPoint(t.position);
+
+		StopCoroutine(TURN_COROUTINE_NAME);
+		StartCoroutine(TURN_COROUTINE_NAME, targetPoint);
+	}
+
+	private IEnumerator TurnTowardsPointAndShootCoroutine(Vector3 point)
+	{
+		turning = true;
+
+		point = point - transform.position;
+		point.z = transform.position.z;
+
+		Quaternion startRotation = transform.rotation;
+		Quaternion endRotation = Quaternion.LookRotation(point, Vector3.back);
+
+		float time = Quaternion.Angle(startRotation, endRotation) / rotationSpeed;
+
+		for (float t =0 ; t < 1.0f ; t += Time.deltaTime/time)
+		{
+			transform.rotation = Quaternion.Slerp (startRotation, endRotation, t);
+			yield return 0;
+		}
+
+		transform.rotation = endRotation;
+		Shoot();
+		turning = false;
+	}
+
+	// Shoot a bullet forward.
+	private void Shoot()
+	{
+		Bullet bullet = PoolManager.Instance.Spawn(Constants.BULLET_PREFAB_NAME).GetComponent<Bullet>();
+		bullet.SetPosition(launcher.position);
+		bullet.SetTrajectory(bullet.transform.position + transform.forward);
+	}
+
+	// Make the ship invincible for a time.
+	private IEnumerator StartInvincibilityTimer(float timeLimit)
+	{
+		GetComponent<Collider2D>().enabled = false;
+
+		SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+		float timer = 0;
+		float blinkSpeed = 0.25f;
+
+		while (timer < timeLimit)
+		{
+			yield return new WaitForSeconds(blinkSpeed);
+
+			spriteRenderer.enabled = !spriteRenderer.enabled;
+			timer += blinkSpeed;
+		}
+
+		spriteRenderer.enabled = true;
+		GetComponent<Collider2D>().enabled = true;
+	}
+
+	
+	#endregion
+}
---
+++ Assets/Scripts/Gameplay/UserInputHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputHandler : MonoBehaviour
+{
+    public delegate void TapAction(Touch t);
+    public static event TapAction OnTap;
+    public float tapMaxMovement = 50f;
+
+    private Vector2 movement;
+    private bool tapGestureFailed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            if (touch.phase == TouchPhase.Began)
+            {
+                movement = Vector2.zero;
+            }
+            else if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                movement += touch.deltaPosition;
+                if (movement.magnitude > tapMaxMovement)
+                {
+                    tapGestureFailed = true;
+                }
+            }
+            else
+            {
+                if (!tapGestureFailed)
+                {
+                    if (OnTap != null) OnTap(touch);
+                }
+                tapGestureFailed = false;
+            }
+        }
+    }
+}
